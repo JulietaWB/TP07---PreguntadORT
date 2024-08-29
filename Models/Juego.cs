946d25f48e1f354839d8cf80bee00984a04445c5
JULIETA WAINHAUS BARBER <48460067@ort>
2024-08-29 15:29:51 -0300

    Avancé con las clases (?

--- Models/Juego.cs
+++ Models/Juego.cs
@@ -1,13 +1,58 @@
 public static class Juego
 {
     //Propiedades
-    private static string username {get;set; }
-    
-    string username
-int puntajeActual
-int contadorPreguntaActual
-Pregunta PreguntaActual
-int cantidadPreguntasCorrectas
-List<Pregunta> ListaPreguntas
-List<Respuesta> ListaRespuestas
+    private static string Username {get;set; }
+    private static int PuntajeActual {get;set; }
+    private static int ContadorPreguntaActual {get;set; }
+    private static Pregunta PreguntaActual {get;set; }
+    private static int CantidadPreguntasCorrectas {get;set; }
+    private static List<Pregunta> ListaPreguntas {get;set; }
+    private static List<Respuesta> ListaRespuestas {get;set; }
+
+
+    // Métodos
+    private void InicializarJuego()
+    {
+        Username="";
+        PuntajeActual=0;
+        ContadorPreguntaActual=1;
+        CantidadPreguntasCorrectas=0;
+    }
+
+    public List<Categoria>  ObtenerCategorias()
+    {
+        List <Categoria> lista = new List <Categoria>();
+        lista=BD.ObtenerCategorias();
+
+        return lista;
+    }
+
+    public List<Dificultad>  ObtenerDificultades()
+    {
+        List <Dificultad> lista = new List <Dificultad>();
+        lista=BD.ObtenerDificultades();
+
+        return lista;
+    }
+
+    public List<Pregunta> CargarPartida(string username, int dificultad, int categoria)
+    {
+        List <Pregunta> lista = new List <Pregunta>();
+        lista=BD.ObtenerPreguntas(dificultad, categoria);
+        Juego.InicializarJuego();
+
+        return lista;
+    }
+
+    public Pregunta ObtenerProximaPregunta()
+    {
+        Pregunta pre = new Pregunta();
+        List <Pregunta> lista = new List <Pregunta>();
+        lista=BD.ObtenerPreguntas(dificultad, categoria)
+        pre=lista[ContadorPreguntaActual];
+
+        return pre;
+    }
+
+
 }
