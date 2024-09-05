d68d5b9aba6427762bcfaa87d0b859d955b91369
JULIETA WAINHAUS BARBER <48460067@ort>
2024-09-05 14:17:45 -0300

    Completé las clases Categoria.cs y Dificultad.cs

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -1,5 +1,16 @@
 public class Categoria
 {
-        
+    private static int IdCategoria {get;set; }
+    private static string Nombre {get;set; }
+    private static string Foto {get;set; }
+
+    public Dificultad (int idCategoria, string nombre, string foto) 
+    {
+        IdCategoria = idCategoria;
+        Nombre = nombre;
+        string Foto = foto;
+    }
+
+    public Dificultad () {  }
 }
 
--- Models/Dificultad.cs
+++ Models/Dificultad.cs
@@ -1,4 +1,14 @@
 public class Dificultad
 {
-    
+    private static int IdDificultad {get;set; }
+    private static string Nombre {get;set; }
+
+
+    public Dificultad (int idDificultad, string nombre) 
+    {
+        IdDificultad = idDificultad;
+        Nombre = nombre;
+    }
+
+    public Dificultad () {  }
 }
