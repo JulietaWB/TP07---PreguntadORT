4b08c23143fc988fe1380e21c15e6727c58c480f
Maia Rasumoff <48387900@est.ort.edu.ar>
2024-09-07 20:34:09 -0300

    pregunta y respuesta

--- Models/Pregunta .cs
+++ Models/Pregunta .cs
@@ -1,4 +1,19 @@
 public class Pregunta
 {
-    
+    private static int IdPregunta {get;set;}
+    private static int IdCategoria {get;set;}
+    private static int IdDificultad {get;set;}
+    private static string Enunciado {get;set;}
+    private static string Foto {get;set;}
+
+    public Pregunta (int Id_Pregunta, int Id_Categoria, int Id_Dificultad, string _Enunciado, string _Foto ) { 
+        IdPregunta = Id_Pregunta;
+        IdCategoria = Id_Categoria;
+        IdDificultad = Id_Dificultad;
+        Enunciado = _Enunciado;
+        Foto = _Foto; 
+     }
+
+    public Pregunta () {  }
+
 }
--- Models/Respuesta.cs
+++ Models/Respuesta.cs
@@ -1,4 +1,22 @@
 public class Respuesta
 {
+    private static int IdRespuesta {get;set;}
+    private static int IdPregunta {get;set;}
+    private static int Opcion {get;set;}
+    private static string Contenido {get;set;}
+    private static bool Correcta {get;set;}
+    private static string Foto {get;set;}
+
+        public Respuesta (int Id_Respuesta, int Id_Pregunta, int _Opcion, string _Contenido, bool _Correcta, string _Foto ) { 
+        IdPregunta = Id_Pregunta;
+        IdRespuesta = Id_Respuesta;
+        Opcion = _Opcion;
+        Correcta = _Correcta;
+        Foto = _Foto; 
+     }
+
+    public Respuesta () {  }
+
     
+
 }
